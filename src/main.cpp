55af70a24047c04e86f8c247b4ea1c4375481c66
unknown <panasonic@PC-DE1901C1486R.japan.gds.panasonic.com>
2020-04-30 10:45:31 +0900

    modify using ESPAsyncWebServer

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /* 
 The MIT License (MIT)
 
-Copyright (c) 2019 riraotech.com
+Copyright (c) 2020 riraotech.com
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -22,15 +22,19 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
 #include <Arduino.h>
 #include <ESP32_SPIFFS_ShinonomeFNT.h>
 #include <ESP32_SPIFFS_UTF8toSJIS.h>
-#include <WiFiClientSecure.h>
 #include <time.h>
 #include <stdio.h>
-#define ARDUINOJSON_DECODE_UNICODE 1
-#include <ArduinoJson.h>
-#include <Wifi.h>
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+#include "SPIFFS.h"
 
 #define JST     3600* 9
 
@@ -57,37 +61,23 @@ const char* UTF8SJIS_file         = "/Utf8Sjis.tbl";  //UTF8 Shift_JIS 変換テ
 const char* Shino_Zen_Font_file   = "/shnmk16.bdf";   //全角フォントファイル名を定義
 const char* Shino_Half_Font_file  = "/shnm8x16.bdf";  //半角フォントファイル名を定義
 
-const char* appid       = "dj00aiZpPU5xUWRpRTlhZXpBMCZzPWNvbnN1bWVyc2VjcmV0Jng9MzY-";//APP ID
-const char* zipcode     = "592-8344";//郵便番号
-const char* output      = "json";//出力形式
-const char* server      = "map.yahooapis.jp";
-const int informPeriod  = 10;//10分間隔で降雨情報を取得
-
-const char* yahooapi_root_ca= \
-     "-----BEGIN CERTIFICATE-----\n" \
-     "MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ\n" \
-     "RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD\n" \
-     "VQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTAwMDUxMjE4NDYwMFoX\n" \
-     "DTI1MDUxMjIzNTkwMFowWjELMAkGA1UEBhMCSUUxEjAQBgNVBAoTCUJhbHRpbW9y\n" \
-     "ZTETMBEGA1UECxMKQ3liZXJUcnVzdDEiMCAGA1UEAxMZQmFsdGltb3JlIEN5YmVy\n" \
-     "VHJ1c3QgUm9vdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKMEuyKr\n" \
-     "mD1X6CZymrV51Cni4eiVgLGw41uOKymaZN+hXe2wCQVt2yguzmKiYv60iNoS6zjr\n" \
-     "IZ3AQSsBUnuId9Mcj8e6uYi1agnnc+gRQKfRzMpijS3ljwumUNKoUMMo6vWrJYeK\n" \
-     "mpYcqWe4PwzV9/lSEy/CG9VwcPCPwBLKBsua4dnKM3p31vjsufFoREJIE9LAwqSu\n" \
-     "XmD+tqYF/LTdB1kC1FkYmGP1pWPgkAx9XbIGevOF6uvUA65ehD5f/xXtabz5OTZy\n" \
-     "dc93Uk3zyZAsuT3lySNTPx8kmCFcB5kpvcY67Oduhjprl3RjM71oGDHweI12v/ye\n" \
-     "jl0qhqdNkNwnGjkCAwEAAaNFMEMwHQYDVR0OBBYEFOWdWTCCR1jMrPoIVDaGezq1\n" \
-     "BE3wMBIGA1UdEwEB/wQIMAYBAf8CAQMwDgYDVR0PAQH/BAQDAgEGMA0GCSqGSIb3\n" \
-     "DQEBBQUAA4IBAQCFDF2O5G9RaEIFoN27TyclhAO992T9Ldcw46QQF+vaKSm2eT92\n" \
-     "9hkTI7gQCvlYpNRhcL0EYWoSihfVCr3FvDB81ukMJY2GQE/szKN+OMY3EU/t3Wgx\n" \
-     "jkzSswF07r51XgdIGn9w/xZchMB5hbgF/X++ZRGjD8ACtPhSNzkE1akxehi/oCr0\n" \
-     "Epn3o0WC4zxe9Z2etciefC7IpJ5OCBRLbf1wbWsaY71k5h+3zvDyny67G7fyUIhz\n" \
-     "ksLi4xaNmjICq44Y3ekQEe5+NauQrz4wlHrQMz2nZQ/1/I6eYs9HRCwBXbsdtTLS\n" \
-     "R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp\n" \
-     "-----END CERTIFICATE-----\n";
+//const char *ssid = "ESP32ap";
+//const char *password = "12345678";
 
 ESP32_SPIFFS_ShinonomeFNT SFR;  //東雲フォントをSPIFFSから取得するライブラリ
-WiFiClientSecure client;
+
+// Replace with your network credentials
+const char* ssid = "Buffalo-G-FAA8";
+const char* password = "34ywce7cffyup";
+
+// Set LED GPIO
+const int ledPin = 2;
+// Stores LED state
+String ledState;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
 
 SemaphoreHandle_t xMutex = NULL;
 
@@ -372,225 +362,8 @@ void printTimeLEDMatrix(){
   printLEDMatrix(sj_length, time_font_buf, time_font_color);
 }
 
-void makeHostStr(String &hostStr){
-  hostStr = "Host: ";
-  hostStr += server;
-}
-
-void makeAgentStr(String &agentStr){
-  agentStr = "User-Agent: Yahoo AppID: ";
-  agentStr += appid;
-}
-
-void getYahooApiJsonInfo(String httpRequest, String &resultJson){
-  String getStr;
-  String hostStr;
-  String agentStr;
-
-  client.setCACert(yahooapi_root_ca);
-
-  Serial.println("\nStarting connection to server...");
-  if (!client.connect(server, 443)){
-    Serial.println("Connection failed!");
-  }else {
-    Serial.println("Connected to server!");
-
-    client.println(httpRequest);
-    client.println();
-
-    delay(200);
-
-    while (client.connected()) {
-      String line = client.readStringUntil('\n');
-      if (line == "\r") {
-        Serial.println("headers received");
-        break;
-      }
-    }
-
-    delay(200);
-
-    while (client.available()) {
-      resultJson += (char)client.read();
-    }
-
-    client.stop();
-  }
-}
-
-void makeGetZipCodeStr(String zipcode, String &getStr){
-    getStr = "GET https://map.yahooapis.jp/search/zip/V1/zipCodeSearch?query=";
-    getStr += zipcode;
-    getStr += "&output=";
-    getStr += output;
-    getStr += "&appid=";
-    getStr += appid;
-    getStr += " HTTP/1.1";
-} 
-
-void makeZipCodeHttpRequestStr(String &httpRequest){
-  String getStr;
-  String hostStr;
-  String agentStr;
-  String coordinates;
-
-  makeGetZipCodeStr(zipcode, getStr);
-  makeHostStr(hostStr);
-  makeAgentStr(agentStr);
-
-  httpRequest = getStr;
-  httpRequest += "\n";
-  //httpRequest += hostStr;
-  //httpRequest += "\n";
-  //httpRequest += agentStr;
-  //httpRequest += "\n";
-  httpRequest += "Connection: close";
-}
-
-void getCoordinatesFromZipcode(String zipcode, String &coordinates){
-  String httpRequest;
-  String resultJson;
-  
-  //httpRequestを作成する
-  makeZipCodeHttpRequestStr(httpRequest);
-
-  Serial.println(httpRequest);
-
-  getYahooApiJsonInfo(httpRequest, resultJson);
-
-  //Serial.println(resultJson);
-
-  const size_t capacity = JSON_ARRAY_SIZE(0) + JSON_ARRAY_SIZE(1) + JSON_ARRAY_SIZE(2) + JSON_ARRAY_SIZE(3) + 6*JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(7) + 2*JSON_OBJECT_SIZE(8) + 3*JSON_OBJECT_SIZE(9) + 990;
-
-  DynamicJsonDocument doc(capacity);
-
-  deserializeJson(doc, resultJson);
-
-  JsonObject Feature_0 = doc["Feature"][0];
-
-  const char* Feature_0_Geometry_Coordinates = Feature_0["Geometry"]["Coordinates"];
-
-  Serial.printf("Coordinates = %s\n", Feature_0_Geometry_Coordinates);
-
-  coordinates = Feature_0_Geometry_Coordinates;
-}
-
-void makeGetStr(String coordinates, String &getStr){
-  getStr = "GET https://map.yahooapis.jp/weather/V1/place?coordinates=";
-  getStr += coordinates;
-  getStr += "&output=";
-  getStr += output;
-  getStr += "&appid=";
-  getStr += appid;
-  getStr += " HTTP/1.1";
-}
-
-void makeWeatherHttpRequestStr(String &httpRequest){
-  String getStr;
-  String hostStr;
-  String agentStr;
-  String coordinates;
-
-  getCoordinatesFromZipcode(zipcode, coordinates);
-
-  makeGetStr(coordinates, getStr);
-  makeHostStr(hostStr);
-  makeAgentStr(agentStr);
-
-  httpRequest = getStr;
-  httpRequest += "\n";
-  //httpRequest += hostStr;
-  //httpRequest += "\n";
-  //httpRequest += agentStr;
-  //httpRequest += "\n";
-  httpRequest += "Connection: close";
-}
-
-void getWeatherStrings(JsonArray &i_weather, int i_index, String &o_type, String &o_date, float &o_rainfall){
-  JsonObject Feature_0_Property_WeatherList_Weather_0 = i_weather[i_index];
-  const char* Type = Feature_0_Property_WeatherList_Weather_0["Type"];
-  const char* Date = Feature_0_Property_WeatherList_Weather_0["Date"];
-  float Rainfall = Feature_0_Property_WeatherList_Weather_0["Rainfall"];
-
-  Serial.printf("Type = %s, Date = %s, Rainfall= %f\n", Type, Date, Rainfall);
-
-  o_type = Type;
-  o_date = Date;
-  o_rainfall = Rainfall;
-
-}
-
-//雨降りの状態
-#define RAINFALL_END    0x01  //降雨が終わる時点
-#define RAINFALL_NO     0x02  //雨が降っていない状態　
-#define RAINFALL_START  0x03  //降雨が開始する時点
-#define RAINFALL_NOW    0x04  //雨が継続的に降っている状態
-
-uint16_t getWeatherInfo(int &forcast_time){
-
-  String weatherJsonInfo;
-  String httpRequest;
-
-  String type;
-  String date;
-  float rainfall = 0;
-  
-  uint16_t weatherInfo = 0;
-
-  makeWeatherHttpRequestStr(httpRequest);
-
-  Serial.println(httpRequest);
-
-  getYahooApiJsonInfo(httpRequest, weatherJsonInfo);
-
-  //Serial.println(weatherJsonInfo);
-
-  const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_ARRAY_SIZE(7) + JSON_OBJECT_SIZE(1) + 3*JSON_OBJECT_SIZE(2) + 7*JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(7) + 660;
-  DynamicJsonDocument doc(capacity);
-
-  deserializeJson(doc, weatherJsonInfo);
-  JsonObject Feature_0 = doc["Feature"][0];
-
-  JsonArray WeatherList = Feature_0["Property"]["WeatherList"]["Weather"];
-  
-  getWeatherStrings(WeatherList, 0, type, date, rainfall);
-
-  //現在雨が降っていない
-  if(rainfall == 0.00){
-    for(int i = 1; i < 7; i++){
-      getWeatherStrings(WeatherList, i, type, date, rainfall);
-      if(rainfall > 0.00){
-        //(i*10-10)分後に雨が降ります。
-        forcast_time = i * 10 - 10;
-        weatherInfo = RAINFALL_START;
-        break;
-      }
-      else{
-        weatherInfo = RAINFALL_NO;
-      }
-    } 
-  }else{//現在雨が降っている
-    for(int i = 1; i < 7; i++){
-      getWeatherStrings(WeatherList, i, type, date, rainfall);
-      if(rainfall == 0.00){
-        //(i*10-10)分後に雨が止みます。
-        forcast_time = i * 10 - 10;
-        weatherInfo = RAINFALL_END;
-        break;
-      }
-      else{
-        //しばらく雨が降ります。６０分後も降雨状態
-        weatherInfo = RAINFALL_NOW;
-      }
-    }
-  }
-
-  return weatherInfo;
-}
-
 portTickType Delay1000 = 1000 / portTICK_RATE_MS; //freeRTOS 用の遅延時間定義
 TaskHandle_t hClock;
-TaskHandle_t hWeatherInfo;
 
 void ClockTask(void *pvParameters) {
   Serial.printf("ClockTask coreID = %d, ClockTask priority = %d\n", xPortGetCoreID(), uxTaskPriorityGet(hClock));
@@ -602,23 +375,10 @@ void ClockTask(void *pvParameters) {
   while(1){
       xStatus = xSemaphoreTake(xMutex, xTicksToWait);
 
-      //Serial.println("check for mutex (ClockTask)");
+      Serial.println("check for mutex (ClockTask)");
 
-      if(xStatus == pdTRUE){
-        time_t t;
-        struct tm *tm;
-
-        t = time(NULL);
-        tm = localtime(&t);
-         
-        if(tm->tm_min % informPeriod == 0 && tm->tm_sec < 3){
-            Serial.printf("tm_sec = %d\n", tm->tm_sec);
-            Serial.println("Give Semaphore(ClockTask)");
-            xSemaphoreGive(xMutex);
-        }
-        else{
+      if(xStatus == pdTRUE){         
           printTimeLEDMatrix();
-        }
       }
 
       //xSemaphoreGive(xMutex);
@@ -636,103 +396,30 @@ void printConnecting(void){
   printLEDMatrix(sj_length, font_buf, font_color1);
 }
 
-void WeatherInfoTask(void *pvParameters){
-  Serial.printf("WeatherInfoTask coreID = %d, WeatherInfoTask priority = %d\n", xPortGetCoreID(), uxTaskPriorityGet(hWeatherInfo));
 
-  BaseType_t xStatus;
-  const TickType_t xTicksToWait = 500UL;
-  xSemaphoreGive(xMutex);
-
-  uint16_t sj_length = 0;//半角文字数 
-    
-  //フォントデータバッファ
-  uint8_t font_buf[100][16] = {0};
-  uint8_t yahoo_font_buf[8][16] = {0};
-
-  //フォント色データ　str1（半角文字毎に設定する）
-  uint8_t font_color1[100] = {G,G,G,G,G,G,G,G,G,G,
-                              G,G,G,G,G,G,G,G,G,G,
-                              G,G,G,G,G,G,G,G,G,G,
-                              G,G,G,G,G,G,G,G,G,G,
-                              G,G,G,G,G,G,G,G,G,G,
-                              G,G,G,G,G,G,G,G,G,G,
-                              G,G,G,G,G,G,G,G,G,G,
-                              G,G,G,G,G,G,G,G,G,G,
-                              G,G,G,G,G,G,G,G,G,G,
-                              G,G,G,G,G,G,G,G,G,G};
-
-  uint8_t yahoo_font_color[8] = {O,O,O,O,O,O,O,O};
-
-  char tmp_str[100] = {0};
-
-  int forcast_time;
-
-  while(1){
-
-    xStatus = xSemaphoreTake(xMutex, xTicksToWait);
-
-    //Serial.println("check for mutex (WeatherInfoTask)");
-
-    if(xStatus == pdTRUE ){
-      
-      printConnecting();
-
-      uint16_t weather_state = getWeatherInfo(forcast_time);
-
-      //ここでビープ音を出音する
-      if(weather_state != RAINFALL_NO){
-        sj_length = SFR.StrDirect_ShinoFNT_readALL(" Yahoo! ", yahoo_font_buf);
-        printLEDMatrix(sj_length, yahoo_font_buf, yahoo_font_color);
-
-        delay(1000);
-
-        sj_length = SFR.StrDirect_ShinoFNT_readALL("気象情報", yahoo_font_buf);
-        printLEDMatrix(sj_length, yahoo_font_buf, yahoo_font_color);
-
-        delay(1000);
-
-        switch(weather_state){
-          case RAINFALL_END:
-            if(forcast_time != 0){
-              sprintf(tmp_str, "        %d分後に雨が止む予報です。", forcast_time);          
-            }else{
-              sprintf(tmp_str, "        すぐに雨が止む予報です。");          
-            }
-          break;
-          case RAINFALL_NO://雨は降っていない。60分後の予報もない
-            sprintf(tmp_str, "        雨が降る予報はありません。");          
-          break;
-          case RAINFALL_START:
-            if(forcast_time != 0){
-              sprintf(tmp_str, "        %d分後に雨が降る予報です。", forcast_time);          
-            }else{
-              sprintf(tmp_str, "        すぐに雨が降る予報です。");          
-            }
-          break;
-          case RAINFALL_NOW:
-            sprintf(tmp_str, "        雨が降っています。しばらく雨が続きます。");    
-          break;
-          default:
-            ;//nothing
-        }
-        Serial.printf("%s\n", tmp_str);
-        sj_length = SFR.StrDirect_ShinoFNT_readALL(tmp_str, font_buf);
-        scrollLEDMatrix(sj_length, font_buf, font_color1, 30);
-      }
+// Replaces placeholder with LED state value
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATE"){
+    if(digitalRead(ledPin)){
+      ledState = "ON";
     }
-
-    xSemaphoreGive(xMutex);
-    delay(1000);
-
+    else{
+      ledState = "OFF";
+    }
+    Serial.print(ledState);
+    return ledState;
   }
+  return String();
 }
+
 
 void setup() {
 
   uint16_t sj_length = 0;//半角文字数 
 
   delay(1000);
-  Serial.begin(115200);
+  //Serial.begin(115200);
   setAllPortOutput();
   setAllPortLow();
 
@@ -746,59 +433,80 @@ void setup() {
 
   SFR.SPIFFS_Shinonome_Init3F(UTF8SJIS_file, Shino_Half_Font_file, Shino_Zen_Font_file);
  
-  // 前回接続時情報で接続する
-  Serial.println("WiFi begin");
-  WiFi.begin();
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+
+  // Initialize SPIFFS
+  if(!SPIFFS.begin(true)){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-
-    // 10秒以上接続できなかったら抜ける
-    if ( 10000 < millis() ) {
-      break;
-    }
-  }
-  Serial.println("");
-
-  // 未接続の場合にはSmartConfig待受
-  if ( WiFi.status() != WL_CONNECTED ) {
-    WiFi.mode(WIFI_STA);
-    WiFi.beginSmartConfig();
-
-    Serial.println("Waiting for SmartConfig");
-    while (!WiFi.smartConfigDone()) {
-      delay(500);
-      Serial.print("#");
-      // 30秒以上接続できなかったら抜ける
-      if ( 30000 < millis() ) {
-        Serial.println("");
-        Serial.println("Reset");
-        ESP.restart();
-      }
-    }
-
-    // Wi-fi接続
-    Serial.println("");
-    Serial.println("Waiting for WiFi");
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-      // 60秒以上接続できなかったら抜ける
-      if ( 60000 < millis() ) {
-        Serial.println("");
-        Serial.println("Reset");
-        ESP.restart();
-      }
-    }
-    Serial.println("");
-    Serial.println("WiFi Connected.");
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
   }
 
-  Serial.println();
-  Serial.printf("Connected, IP address: ");
+  // Print ESP32 Local IP Address
   Serial.println(WiFi.localIP());
 
-  sj_length = SFR.StrDirect_ShinoFNT_readALL("        WiFi Connected.", font_buf);
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+
+  // Route to set GPIO to HIGH
+  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(ledPin, HIGH);    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // Route to set GPIO to LOW
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(ledPin, LOW);    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+
+  // Start server
+  server.begin();
+
+
+  // Serial.println();
+  // Serial.print("Configuring access point...");
+  // /* You can remove the password parameter if you want the AP to be open. */
+  // //アクセスポイントを起動する
+  // WiFi.softAP(ssid, password);
+
+  // IPAddress myIP = WiFi.softAPIP();
+  // Serial.print("AP IP address: ");
+  // Serial.println(myIP);
+  // //
+  // //サーバーセットアップ
+  // //
+  // /* Set up mDNS */
+  // if (!MDNS.begin("esp32")) {
+  //     Serial.println("Error setting up MDNS responder!");
+  //     while(1) {
+  //         delay(1000);
+  //     }
+  // }
+  // Serial.println("mDNS responder started");
+  // /* Start Web Server server */
+  // server.begin();
+  // //Serial.println("Web server started");
+
+  // /* Add HTTP service to MDNS-SD */
+  // MDNS.addService("http", "tcp", 80);
+
+  sj_length = SFR.StrDirect_ShinoFNT_readALL("        Web server started.", font_buf);
   scrollLEDMatrix(sj_length, font_buf, font_color1, 30);
 
   sj_length = SFR.StrDirect_ShinoFNT_readALL("        "+ WiFi.localIP().toString(), font_buf);
@@ -810,9 +518,9 @@ void setup() {
   xMutex = xSemaphoreCreateMutex();
 
   if( xMutex != NULL ){
-    xTaskCreatePinnedToCore(ClockTask, "ClockTask", 4096, NULL, 1, &hClock, 1); //ClockTask開始
-    xTaskCreatePinnedToCore(WeatherInfoTask, "WeatherInfoTask", 8192, NULL, 6, &hWeatherInfo, 1); //WeatherInfoTask開始
-  }else{
+    //xTaskCreatePinnedToCore(ClockTask, "ClockTask", 4096, NULL, 1, &hClock, 1); //ClockTask開始
+  }
+  else{
     while(1){
         Serial.println("rtos mutex create error, stopped");
         delay(1000);
