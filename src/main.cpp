dec5ac937d1a7e94e85f5ed9aed0a7c9d7af1a49
unknown <panasonic@PC-DE1901C1486R.japan.gds.panasonic.com>
2020-05-13 23:57:26 +0900

    modify for OTA

--- src/main.cpp
+++ src/main.cpp
@@ -26,11 +26,13 @@ SOFTWARE.
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 #include <Servo.h>
 
-#define ESP32_AP_MODE true
+#define ESP32_AP_MODE false
 
 #if ESP32_AP_MODE 
 const char *ssid = "ESP32-G-AP";
@@ -91,40 +93,24 @@ void setup()
     return;
   }
 
+
 #if (ESP32_AP_MODE == false)
   // Connect to Wi-Fi
+  ArduinoOTA.setHostname("esp32");
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
   }
-
 #else
   Serial.println();
   Serial.println("Configuring access point...");
 
   //アクセスポイントを起動する
   WiFi.softAP(ssid, password);
-
   IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
-
-  /* Set up mDNS */
-  //http://esp32.local/
-  if (!MDNS.begin("esp32"))
-  {
-    Serial.println("Error setting up MDNS responder!");
-    while (1)
-    {
-      delay(1000);
-    }
-  }
-  Serial.println("mDNS responder started");
-
-  // Print ESP32 Local IP Address
-  Serial.println(myIP.toString());
 #endif
 
   // Route for root / web page
@@ -159,6 +145,34 @@ void setup()
   //Webサーバー開始
   server.begin();
 
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
 #if (ESP32_AP_MODE == flase)
   Serial.println("[Client] Mode");
   Serial.println("Web server started.");
@@ -168,6 +182,9 @@ void setup()
   Serial.println("Web server started.");
   Serial.println(myIP.toString());
 #endif
+  
+  Serial.print("Hostname: ");
+  Serial.println(ArduinoOTA.getHostname());
 
   myservo.write(0);
 }
@@ -203,5 +220,6 @@ void loop()
       ;
   }
 
-  delay(1);
+  ArduinoOTA.handle();
+
 }
